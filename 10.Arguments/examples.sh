49d97c60e5be683b0e3c73dcc083c49717737e35
PrameshwarThapa <thapaprameshwar7@gmail.com>
2025-05-30 12:09:03 +1000

    Argument examples

--- 10.Arguments/examples.sh
+++ 10.Arguments/examples.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
 # # Example 1. Simple Gretting Script
-# echo "Hello, $1!"
+ echo "Hello, $1!"
 
 # Example 2. Checking Number of Arguments
-# if [ $# -ne 2 ]; then
-#     echo " Error: you need to provide exactly two arguments"
-#     echo "Usage: $0 arg1  arg2"
-#     exit 1
-# fi
-# echo " First argument: $1"
-# echo "Second argument: $2"
+if [ $# -ne 2 ]; then
+    echo " Error: you need to provide exactly two arguments"
+    echo "Usage: $0 arg1  arg2"
+    exit 1
+fi
+echo " First argument: $1"
+echo "Second argument: $2"
 
 # Example 3. Looping through all arguments
-#  echo " You passed $# arguments."
+ echo " You passed $# arguments."
 
-#  for arg in "$@"
-#  do
-#     echo "Arguments:$arg"
-# done
+ for arg in "$@"
+ do
+    echo "Arguments:$arg"
+done
 
 # Example 4. Combine All arguments as Single String
-# echo "Using \"\$*\":"
-# echo "$*"             
-# echo
+echo "Using \"\$*\":"
+echo "$*"             
+echo
 
-# echo "Using \"\$@\":"
-# for arg in "$@"
-# do
-#     echo "$arg"    
-# done
+echo "Using \"\$@\":"
+for arg in "$@"
+do
+    echo "$arg"    
+done
 
 # Example 5. Using Arguments for Conditional Logic
 if [ -z "$1" ]; then            
